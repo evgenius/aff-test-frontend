8b3fb9eeaaab8a242a2131a685ada566798f4a40
Evgeniy Abduzhapparov <abd.evg@gmail.com>
2017-10-21 21:46:53 +0200

    Add a new layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MatCheckboxModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatSlideToggleModule,
+         MatButtonModule, MatGridListModule,
+         MatExpansionModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MatchesComponent } from './matches.component';
 import { MatchService } from './match.service';
+import { SliderModule } from './modules/slider/slider.module';
 
 @NgModule({
   declarations: [
@@ -17,8 +21,13 @@ import { MatchService } from './match.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MatCheckboxModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSlideToggleModule,
     MatButtonModule,
+    MatExpansionModule,
+    SliderModule,
   ],
   providers: [MatchService],
   bootstrap: [AppComponent]
