56257d33339a857f0b64ddf4397f791a9574314a
Evgeniy Abduzhapparov <abd.evg@gmail.com>
2017-10-21 21:43:53 +0200

    Add the rest filters

--- src/app/filter.ts
+++ src/app/filter.ts
@@ -19,3 +19,31 @@ export class FavouriteFilter implements Filter {
     return "favourite";
   }
 }
+
+export class CompatibilityFilter implements Filter {
+  constructor(private minScore: number, private maxScore: number) {}
+  getValue(): string {
+    return "compatibility:" + this.minScore + ":" + this.maxScore;
+  }
+}
+
+export class AgeFilter implements Filter {
+  constructor(private minAge: number, private maxAge: number) {}
+  getValue(): string {
+    return "age:" + this.minAge + ":" + this.maxAge;
+  }
+}
+
+export class HeightFilter implements Filter {
+  constructor(private minHeight: number, private maxHeight: number) {}
+  getValue(): string {
+    return "height:" + this.minHeight + ":" + this.maxHeight;
+  }
+}
+
+export class DistanceFilter implements Filter {
+  constructor(private lat: string, private lon: string, private distance: string) {}
+  getValue(): string {
+    return "distance:" + this.lat + ":" + this.lon + ":" + this.distance;
+  }
+}
--- src/app/matches.component.ts
+++ src/app/matches.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { Match } from './match'
 import { MatchService } from './match.service';
-import { Filter, PhotoFilter, ContactFilter, FavouriteFilter } from './filter'
+import { Filter, PhotoFilter, ContactFilter, FavouriteFilter,
+         CompatibilityFilter, AgeFilter, DistanceFilter } from './filter'
+
+import { SliderRange } from './modules/slider/range';
+
+declare var jquery: any;
+declare var $: any;
+
+var MIN_AGE = 18;
+var MAX_AGE = 95;
+
+var MIN_COMP = 1;
+var MAX_COMP = 99;
+
+var MIN_HEIGHT = 135;
+var MAX_HEIGHT = 210;
+
+var AGES = {};
+for (var i = MIN_AGE; i <= MAX_AGE; i++) {
+  AGES[i] = i;
+}
+AGES['> ' + MAX_AGE] = -1;
+
+var COMPS = {};
+for (var i = MIN_COMP; i <= MAX_COMP; i++) {
+  COMPS[i + '%'] = i;
+}
+
+var HEIGHTS = {};
+HEIGHTS['< ' + MIN_HEIGHT + ' cm'] = 0;
+for (var i = MIN_HEIGHT; i <= MAX_HEIGHT; i++) {
+  HEIGHTS[i + ' cm'] = i;
+}
+HEIGHTS['> ' + MAX_HEIGHT + ' cm'] = -1;
+
+
+var DISTANCES = {
+  '< 30 km': 30,
+  '100 km': 100,
+  '200 km': 200,
+  '300 km': 300,
+  '> 300 km': -1,
+};
 
 @Component({
   selector: 'matches',
@@ -16,9 +57,21 @@ import { Filter, PhotoFilter, ContactFilter, FavouriteFilter } from './filter'
 export class MatchesComponent implements OnInit {
   matches: Match[];
   selectedMatch: Match;
+
   enablePhotoFilter: boolean = false;
   enableContactFilter: boolean = false;
   enableFavouriteFilter: boolean = false;
+
+  comps = Object.keys(COMPS);
+  ages = Object.keys(AGES);
+  heights = Object.keys(HEIGHTS);
+  distances = Object.keys(DISTANCES);
+
+  comp: SliderRange;
+  age: SliderRange;
+  height: SliderRange;
+  distance: SliderRange;
+
   filters: Filter[] = [];
 
   constructor(private matchService: MatchService) { }
@@ -43,6 +96,15 @@ export class MatchesComponent implements OnInit {
     }
     if (this.enableFavouriteFilter) {
       this.filters.push(new FavouriteFilter());
+    }
+    if (this.comp) {
+      this.filters.push(new CompatibilityFilter(this.comp.from, this.comp.to));
+    }
+    if (this.age) {
+      this.filters.push(new AgeFilter(this.age.from, this.age.to));
+    }
+    if (this.distance) {
+      this.filters.push(new DistanceFilter('0', '0', DISTANCES[this.distance.from]));
     }
     this.getMatches();
   }
